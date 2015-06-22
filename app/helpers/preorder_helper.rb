dec1e39ee69e598c845a99c4d455bbcc9c032f29
vampaynani <vampay@digitaldealers.mx>
2015-06-22 03:06:35 -0500

    Twit button fixed with twitter_username

--- app/helpers/preorder_helper.rb
+++ app/helpers/preorder_helper.rb
@@ -8,7 +8,7 @@ module PreorderHelper
   end
   def tweet_button
     tweet_text = "I'm #{Settings.primary_stat_verb} number #{number_with_delimiter Order.backers, :delimiter => ","} #{Settings.tweet_text}!"
-    raw "<a href='https://twitter.com/share?url=/' id='tweet_button' class='twitter-share-button twitter-button' data-url=#{request.scheme}//#{request.host}' data-via='#{Settings.product_name}' data-lang='en' data-count='vertical' data-text=\"#{tweet_text}\">Tweet</a>"
+    raw "<a href='https://twitter.com/share?url=/' id='tweet_button' class='twitter-share-button twitter-button' data-url=#{request.scheme}//#{request.host}' data-via='#{Settings.twitter_username}' data-lang='en' data-count='vertical' data-text=\"#{tweet_text}\">Tweet</a>"
   end
 
   def video_url
